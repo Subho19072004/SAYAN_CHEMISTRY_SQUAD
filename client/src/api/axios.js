58c2d7bc889cc54c5f1325c2bb78e55982fff715
Subho Das <subhodas1907@gmail.com>
2026-08-14 10:44:55 +0530

    Connect frontend to production API

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,9 @@
 // api/axios.js
 import axios from "axios";
-export default axios.create({
-  baseURL: "http://localhost:5000/api",
-  withCredentials: true, // sends the httpOnly cookie
+
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true,
 });
+
+export default API;
